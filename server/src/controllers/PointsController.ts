cca39b11bc7d1467a1e2b496c176294dd063848e
Camatta <felipecamattabergi@gmail.com>
2020-06-05 20:48:27 -0300

    Adicionado upload de imagem (backend/frontend)

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -18,7 +18,14 @@ class PointsController {
       .distinct()
       .select('points.*')
 
-    return response.json(points)
+    const serializedPoints = points.map(point => {
+      return {
+        ...point,
+        image_url: `http://10.0.1.102:3333/uploads/${point.image}`
+      }
+    })
+
+    return response.json(serializedPoints)
   }
 
   async store(request: Request, response: Response) {
@@ -36,7 +43,7 @@ class PointsController {
     const trx = await knex.transaction()
 
     const point = {
-      image: 'image-fake',
+      image: request.file.filename,
       name,
       email,
       whatsapp,
@@ -50,12 +57,15 @@ class PointsController {
 
     const point_id = insertedIds[0]
 
-    const pointItems = items.map((item_id: number) => {
-      return {
-        item_id,
-        point_id
-      }
-    })
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id
+        }
+      })
 
     await trx('point_items').insert(pointItems)
 
@@ -76,13 +86,18 @@ class PointsController {
       return response.status(400).json({ message: 'Point not found.' })
     }
 
+    const serializedPoint = {
+      ...point,
+      image_url: `http://10.0.1.102:3333/uploads/${point.image}`
+    }
+
     const items = await knex('items')
       .join('point_items', 'items.id', '=', 'point_items.item_id')
       .where('point_items.point_id', id)
       .select('items.title')
 
     return response.json({
-      point,
+      point: serializedPoint,
       items
     })
   }
